510a51396a9fce03fda14f93b7bef92fc9d82b17
anuragdwi55 <joeygoldberg69@gmail.com>
2025-12-20 01:41:10 +0530

    frontend file changes

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -58,8 +58,19 @@ const api = axios.create({ baseURL: API_BASE })
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) {
-    config.headers = config.headers || {}
-    config.headers.Authorization = `Bearer ${token}`
+    api.interceptors.request.use((config) => {
+      const token = localStorage.getItem('token')
+
+      if (token) {
+        config.headers = {
+          ...(config.headers ?? {}),
+          Authorization: `Bearer ${token}`,
+        }
+      }
+
+      return config
+    })
+
   }
   return config
 })
