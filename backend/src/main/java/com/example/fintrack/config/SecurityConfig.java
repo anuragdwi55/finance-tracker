d6e77a51cddda89c3e0538fe6b5e1946d08403b7
anuragdwi55 <joeygoldberg69@gmail.com>
2025-12-20 15:04:58 +0530

    enabling cors via spring security

--- backend/src/main/java/com/example/fintrack/config/SecurityConfig.java
+++ backend/src/main/java/com/example/fintrack/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+        http    .cors(c -> {} )
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(reg -> reg
